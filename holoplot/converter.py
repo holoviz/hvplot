eacc7e98cfc4b986a02d7816833611d935901f3a
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-05-03 04:13:32 +0100

    Removed index and columns arguments

--- holoplot/converter.py
+++ holoplot/converter.py
@@ -117,9 +117,9 @@ class HoloViewsConverter(param.Parameterized):
 
     _gridded_types = ['image', 'contour', 'contourf', 'quadmesh']
 
-    _data_options = ['x', 'y', 'kind', 'by', 'columns', 'index', 'use_index',
-                     'use_dask', 'dynamic', 'crs', 'value_label',
-                     'group_label', 'backlog', 'persist']
+    _data_options = ['x', 'y', 'kind', 'by', 'use_index', 'use_dask',
+                     'dynamic', 'crs', 'value_label', 'group_label',
+                     'backlog', 'persist']
 
     _axis_options = ['width', 'height', 'shared_axes', 'grid', 'legend',
                      'rot', 'xlim', 'ylim', 'xticks', 'yticks', 'colorbar',
@@ -136,11 +136,11 @@ class HoloViewsConverter(param.Parameterized):
         'hexbin' : ['C', 'reduce_function', 'gridsize']
     }
 
-    def __init__(self, data, x, y, kind=None, by=None, columns=None,
-                 index=None, use_index=True, group_label='Group',
-                 value_label='value', backlog=1000, persist=False,
-                 use_dask=False, crs=None, fields={}, groupby=None,
-                 dynamic=True, width=700, height=300, shared_axes=False,
+    def __init__(self, data, x, y, kind=None, by=None, use_index=True,
+                 group_label='Group', value_label='value',
+                 backlog=1000, persist=False, use_dask=False,
+                 crs=None, fields={}, groupby=None, dynamic=True,
+                 width=700, height=300, shared_axes=False,
                  grid=False, legend=True, rot=None, title=None,
                  xlim=None, ylim=None, xticks=None, yticks=None,
                  logx=False, logy=False, loglog=False, hover=True,
@@ -198,8 +198,6 @@ class HoloViewsConverter(param.Parameterized):
                 self.data = dataset
                 if len(dims) > 2 and not groupby:
                     groupby = [d for d in dims if d not in (x, y)]
-                if not columns:
-                    columns = []
             else:
                 if use_dask:
                     dataset = dataset if isinstance(dataset, xr.Dataset) else dataset.to_dataset()
@@ -208,12 +206,12 @@ class HoloViewsConverter(param.Parameterized):
                     data = dataset.to_dataframe()
                     if len(data.index.names) > 1:
                         data = data.reset_index()
-                if not columns:
-                    columns = data_vars
-                if not index and not x:
-                    index = data_vars[0] if y and y not in data_vars else dims[0]
+                if not y:
+                    y = data_vars
+                if not x:
+                    x = dims[0]
                 if by is None:
-                    by = [c for c in dims if c not in (x, index)]
+                    by = [c for c in dims if c != x]
                 self.data = data
         else:
             raise ValueError('Supplied data type %s not understood' % type(data).__name__)
@@ -235,6 +233,7 @@ class HoloViewsConverter(param.Parameterized):
                                  'could not be found, expected one or '
                                  'more of: %s' % (not_found, list(dataset.coords)))
         else:
+            # Determine valid indexes
             if isinstance(self.data, pd.DataFrame):
                 if self.data.index.names == [None]:
                     indexes = [self.data.index.name or 'index']
@@ -242,6 +241,13 @@ class HoloViewsConverter(param.Parameterized):
                     indexes = self.data.index.names
             else:
                 indexes = [data.index.name or 'index']
+
+            # Rename non-string columns
+            renamed = {c: str(c) for c in data.columns if not isinstance(c, hv.util.basestring)}
+            if renamed:
+                self.data = self.data.rename(columns=renamed)
+
+            # Reset groupby dimensions
             groupby_index = [g for g in groupby if g in indexes]
             if groupby_index:
                 self.data = self.data.reset_index(groupby_index)
@@ -252,12 +258,15 @@ class HoloViewsConverter(param.Parameterized):
                                  'more of: %s' % (not_found, list(self.data.columns)))
 
         # Data-level options
+        self.x = x
+        self.y = y
         self.kind = kind or 'line'
         self.use_dask = use_dask
-        self.index = index
-        self.columns = columns
-        self.use_index = use_index or index
-        if not by:
+        self.use_index = use_index
+        if isinstance(by, (np.ndarray, pd.Series)):
+            self.data['by'] = by
+            self.by = ['by']
+        elif not by:
             self.by = []
         else:
             self.by = by if isinstance(by, list) else [by]
@@ -273,6 +282,7 @@ class HoloViewsConverter(param.Parameterized):
         self.rasterize = rasterize
 
         # By type
+        self.subplots = subplots
         self._by_type = NdLayout if subplots else NdOverlay
 
         # Process style options
@@ -422,55 +432,50 @@ class HoloViewsConverter(param.Parameterized):
 
         data = self.data if data is None else data
         ys = [y]
-        if 'c' in self.kwds and self.kwds['c'] in data.columns:
-            ys += [self.kwds['c']]
+        for p in 'cs':
+            if p in self.kwds and self.kwds[p] in data.columns:
+                ys += [self.kwds[p]]
 
         if self.by:
-            chart = Dataset(data, self.by+[x], ys).to(element, x, ys, self.by).overlay()
+            chart = Dataset(data, self.by+[x], ys).to(element, x, ys, self.by).relabel(**self._relabel)
+            chart = chart.layout() if self.subplots else chart.overlay()
         else:
-            chart = element(data, x, ys)
-        return chart.redim.range(**ranges).redim(**self._redim).relabel(**self._relabel).opts(opts)
+            chart = element(data, x, ys).relabel(**self._relabel)
+        return chart.redim.range(**ranges).redim(**self._redim).opts(opts)
 
+    def _process_args(self, data, x, y):
+        data = (self.data if data is None else data)
+        x = x or self.x
+        if not x and self.use_index:
+            x = data.index.name or 'index'
+        elif not x:
+            raise ValueError('Could not determine what to plot. Expected '
+                             'x to be declared or use_index to be enabled.')
+        y = y or self.y
+        if not y:
+            if len(data.columns) == 1:
+                y = data.columns[0]
+            else:
+                y = [c for c in data.columns if c != x]
+        return data, x, y
 
     def chart(self, element, x, y, data=None):
         "Helper method for simple x vs. y charts"
-        data = (self.data if data is None else data)
-        if not x and not y and len(data.columns) == 1:
-            x = self.index or (data.index.name or 'index')
-            y = data.columns[0]
-        if (x or self.index) and y:
-            return self.single_chart(element, x or self.index, y, data)
-        elif (x or self.index) and self.columns and len(self.columns) == 1:
-            return self.single_chart(element, x or self.index, self.columns[0], data)
+        data, x, y = self._process_args(data, x, y)
+        if x and y and not isinstance(y, (list, tuple)):
+            return self.single_chart(element, x, y, data)
+        elif x and y and len(y) == 1:
+            return self.single_chart(element, x, y[0], data)
 
-        # Note: Loading dask dataframe into memory due to rename bug
-        if self.use_dask: data = data.compute()
         opts = dict(plot=dict(self._plot_opts, labelled=['x']),
                     norm=self._norm_opts, style=self._style_opts)
-
-        if self.use_index or x:
-            if self.index or x:
-                x = x or self.index
-            elif self.use_index:
-                x = data.index.name or 'index'
-            columns = [c for c in self.columns or data.columns if c != x]
-            renamed = {c: 'Dimension_%d' % c for c in columns if isinstance(c, int)}
-            if renamed:
-                data = data.rename(columns=renamed)
-            charts = {}
-            for c in columns:
-                c = renamed.get(c, c)
-                chart = element(data, x, c).redim(**{c: self.value_label})
-                ranges = {x: self._dim_ranges['x'], self.value_label: self._dim_ranges['y']}
-                charts[c] = (chart.relabel(**self._relabel)
-                             .redim.range(**ranges).opts(**opts))
-            if len(charts) == 1:
-                return charts[c].redim(**{self.value_label: c})
-            return self._by_type(charts, self.group_label)
-        else:
-            raise ValueError('Could not determine what to plot. Expected '
-                             'either x and y parameters to be declared '
-                             'or use_index to be enabled.')
+        charts = {}
+        for c in y:
+            chart = element(data, x, c).redim(**{c: self.value_label})
+            ranges = {x: self._dim_ranges['x'], self.value_label: self._dim_ranges['y']}
+            charts[c] = (chart.relabel(**self._relabel)
+                         .redim.range(**ranges).opts(**opts))
+        return self._by_type(charts, self.group_label)
 
     def line(self, x, y, data=None):
         return self.chart(Curve, x, y, data)
@@ -496,49 +501,39 @@ class HoloViewsConverter(param.Parameterized):
     #  Categorical charts    #
     ##########################
 
-    def _category_plot(self, element, data=None):
+    def _category_plot(self, element, x, y, data):
         """
         Helper method to generate element from indexed dataframe.
         """
-        data = self.data if data is None else data
-        if self.index:
-            index = self.index
-        elif self.use_index:
-            index = data.index.name or 'index'
-
-        id_vars = [index]
         opts = {'plot': dict(self._plot_opts, labelled=[]),
                 'style': dict(self._style_opts),
                 'norm': self._norm_opts}
         ranges = {self.value_label: self._dim_ranges['y']}
 
-        if self.columns:
-            data = data[self.columns+id_vars]
+        id_vars = [x]
+        if any((v in [data.index.names]+['index']) or v == data.index.name for v in id_vars):
+            data = data.reset_index()
+        data = data[y+[x]]
 
         if check_library(data, 'dask'):
             from dask.dataframe import melt
         else:
             melt = pd.melt
 
-        if any((v in [data.index.names]+['index']) or v == data.index.name for v in id_vars):
-            data = data.reset_index()
-        df = melt(data, id_vars=id_vars, var_name=self.group_label,
-                  value_name=self.value_label)
-        kdims = [index, self.group_label]
+        df = melt(data, id_vars=[x], var_name=self.group_label, value_name=self.value_label)
+        kdims = [x, self.group_label]
         return (element(df, kdims, self.value_label).redim.range(**ranges)
                 .redim(**self._redim).relabel(**self._relabel).opts(**opts))
 
     def bar(self, x, y, data=None):
-        if x and y:
+        data, x, y = self._process_args(data, x, y)
+        if x and y and not isinstance(y, (list, tuple)):
             return self.single_chart(Bars, x, y, data)
-        elif self.use_index:
-            stack_index = 1 if self.stacked else None
-            opts = {'Bars': {'stack_index': stack_index}}
-            return self._category_plot(Bars, data).opts(plot=opts)
-        else:
-            raise ValueError('Could not determine what to plot. Expected '
-                             'either x and y parameters to be declared '
-                             'or use_index to be enabled.')
+        elif x and y and len(y) == 1:
+            return self.single_chart(Bars, x, y[0], data)
+        stack_index = 1 if self.stacked else None
+        opts = {'Bars': {'stack_index': stack_index}}
+        return self._category_plot(Bars, x, list(y), data).opts(plot=opts)
 
     def barh(self, x, y, data=None):
         return self.bar(x, y, data).opts(plot={'Bars': dict(invert_axes=True)})
@@ -551,20 +546,18 @@ class HoloViewsConverter(param.Parameterized):
         """
         Helper method to generate element from indexed dataframe.
         """
-        data = self.data if data is None else data
+        data, x, y = self._process_args(data, None, y)
 
         opts = {'plot': dict(self._plot_opts, labelled=[]),
                 'norm': self._norm_opts, 'style': self._style_opts}
-        if y:
+        if not isinstance(y, (list, tuple)):
             ranges = {y: self._dim_ranges['y']}
             return (element(data, self.by, y).redim.range(**ranges)
                 .relabel(**self._relabel).opts(**opts))
 
         kdims = [self.group_label]
         ranges = {self.value_label: self._dim_ranges['y']}
-        if self.columns:
-            data = data[self.columns]
-
+        data = data[list(y)]
         if check_library(data, 'dask'):
             from dask.dataframe import melt
         else:
@@ -584,6 +577,8 @@ class HoloViewsConverter(param.Parameterized):
         return self._stats_plot(Violin, y, data)
 
     def hist(self, x, y, data=None):
+        data, x, y = self._process_args(data, x, y)
+
         plot_opts = dict(self._plot_opts)
         opts = dict(plot=dict(plot_opts, labelled=['x']), style=self._style_opts,
                     norm=self._norm_opts)
@@ -591,54 +586,51 @@ class HoloViewsConverter(param.Parameterized):
                      'bin_range': self.kwds.get('bin_range', None),
                      'normed': self.kwds.get('normed', False)}
 
-        data = self.data if data is None else data
-        if y and self.by:
+        if not isinstance(y, (list, tuple)):
             ds = Dataset(data, self.by, y)
-            return histogram(ds.to(Dataset, [], y, self.by), **hist_opts).\
-                overlay().opts({'Histogram': opts})
-        elif y or len(data.columns) == 1:
-            y = y or data.columns[0]
-            ds = Dataset(data, [], y)
-            hist = histogram(ds, dimension=y, **hist_opts).opts({'Histogram': opts})
+            hist = hists = histogram(ds.to(Dataset, [], y, self.by), **hist_opts)
+            if self.by:
+                hist = hists.last
+                hists = hists.layout() if self.subplots else hists.overlay()
             ranges = {hist.kdims[0].name: self._dim_ranges['x'],
                       hist.vdims[0].name: self._dim_ranges['y']}
-            return hist.redim(**self._redim).redim.range(**ranges)
+            return hists.opts({'Histogram': opts}).redim(**self._redim).redim.range(**ranges)
 
         ds = Dataset(data)
         hists = {}
-        columns = self.columns or data.columns
-        for col in columns:
+        for col in y:
             hist = histogram(ds, dimension=col, **hist_opts)
             ranges = {hist.kdims[0].name: self._dim_ranges['x'],
                       hist.vdims[0].name: self._dim_ranges['y']}
             hists[col] = (hist.redim.range(**ranges)
                           .relabel(**self._relabel).opts(**opts))
-        return NdOverlay(hists).redim(**self._redim)
+        return self._by_type(hists).redim(**self._redim)
 
     def kde(self, x, y, data=None):
-        data = self.data if data is None else data
+        data, x, y = self._process_args(data, x, y)
         opts = dict(plot=self._plot_opts, style=self._style_opts, norm=self._norm_opts)
         opts = {'Distribution': opts, 'Area': opts,
                 'NdOverlay': {'plot': dict(self._plot_opts, legend_limit=0)}}
 
-        if y and self.by:
-            ds = Dataset(data)
-            return ds.to(Distribution, y, [], self.by).redim(**self._redim).overlay().opts(opts)
-        elif y or len(data.columns) == 1:
-            y = y or data.columns[0]
-            return Distribution(data, y, []).redim(**self._redim).opts(opts)
-
-        if self.columns:
-            data = data[self.columns]
-        df = pd.melt(data, var_name=self.group_label, value_name=self.value_label)
-        ds = Dataset(df)
-        if len(df):
-            overlay = ds.to(Distribution, self.value_label).overlay()
+        if not isinstance(y, (list, tuple)):
+            ranges = {y: self._dim_ranges['x']}
+            if self.by:
+                dists = Dataset(data).to(Distribution, y, [], self.by)
+                dists = dists.layout() if self.subplots else dists.overlay()
+            else:
+                dists = Distribution(data, y, [])
         else:
-            vdim = self.value_label + ' Density'
-            overlay = NdOverlay({0: Area([], self.value_label, vdim)},
-                                [self.group_label])
-        return overlay.redim(**self._redim).relabel(**self._relabel).opts(opts)
+            ranges = {self.value_label: self._dim_ranges['x']}
+            data = data[y]
+            df = pd.melt(data, var_name=self.group_label, value_name=self.value_label)
+            ds = Dataset(df)
+            if len(df):
+                dists = ds.to(Distribution, self.value_label).overlay()
+            else:
+                vdim = self.value_label + ' Density'
+                dists = NdOverlay({0: Area([], self.value_label, vdim)},
+                                  [self.group_label])
+        return dists.redim(**self._redim).redim.range(**ranges).relabel(**self._relabel).opts(opts)
 
 
     ##########################
@@ -684,6 +676,8 @@ class HoloViewsConverter(param.Parameterized):
     def image(self, x=None, y=None, z=None, data=None):
         import xarray as xr
         data = self.data if data is None else data
+        x = x or self.x
+        y = y or self.y
         if not (x and y):
             x, y = list(data.dims)[::-1]
         if not z:
@@ -705,6 +699,8 @@ class HoloViewsConverter(param.Parameterized):
     def quadmesh(self, x=None, y=None, z=None, data=None):
         import xarray as xr
         data = self.data if data is None else data
+        x = x or self.x
+        y = y or self.y
         if not (x and y):
             x, y = list([k for k, v in data.coords.items() if v.size > 1])
         if not z:
