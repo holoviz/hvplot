860b85f9f5914b8ef8e70d474173c6d9111a0242
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-06-04 14:46:13 +0100

    Fixed dask auth issue in Plotting notebook

--- holoplot/xarray.py
+++ holoplot/xarray.py
@@ -2,7 +2,7 @@ import xarray as xr
 
 from . import HoloPlot, _hv
 
-if _hv.extension._loaded:
+if not _hv.extension._loaded:
     _hv.extension('bokeh', logo=False)
 
 @xr.register_dataset_accessor('holoplot')
