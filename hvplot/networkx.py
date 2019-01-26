9f9a19492a8b3bc5bcc9987d1c790d6c3c540e4a
Philipp Rudiger <prudiger@anaconda.com>
2019-01-26 01:28:38 +0000

    Updates and fixes for NetworkX API

--- hvplot/networkx.py
+++ hvplot/networkx.py
@@ -1,14 +1,115 @@
+from collections import defaultdict
+
 import numpy as np
 import networkx as nx
 import holoviews as _hv
 
+from bokeh.models import HoverTool
 from holoviews import Graph, Labels, dim
+from holoviews.core.util import dimension_sanitizer
 from holoviews.plotting.bokeh import GraphPlot, LabelsPlot
 
+from .util import process_crs
 from .utilities import save, show # noqa
 
 if _hv.extension and not getattr(_hv.extension, '_loaded', False):
     _hv.extension('bokeh', logo=False)
+
+
+def _from_networkx(G, positions, nodes=None, cls=Graph, **kwargs):
+    """
+    Generate a Graph element from a networkx.Graph object and networkx
+    layout function or dictionary of node positions.  Any keyword
+    arguments will be passed to the layout function. By default it
+    will extract all node and edge attributes from the networkx.Graph
+    but explicit node information may also be supplied. Any non-scalar
+    attributes, such as lists or dictionaries will be ignored.
+
+    Parameters
+    ----------
+    G : networkx.Graph
+       Graph to convert to Graph element
+    positions : dict or callable
+       Node positions defined as a dictionary mapping from node id to
+       (x, y) tuple or networkx layout function which computes a
+       positions dictionary.
+    kwargs : dict
+       Keyword arguments for the element
+
+    Returns
+    -------
+    graph : holoviews.Graph
+       Graph element
+    """
+
+    # Unpack edges
+    edges = defaultdict(list)
+    for start, end in G.edges():
+        for attr, value in sorted(G.adj[start][end].items()):
+            if isinstance(value, (list, dict)):
+                continue # Cannot handle list or dict attrs
+            edges[attr].append(value)
+
+        # Handle tuple node indexes (used in 2D grid Graphs)
+        if isinstance(start, tuple):
+            start = str(start)
+        if isinstance(end, tuple):
+            end = str(end)
+        edges['start'].append(start)
+        edges['end'].append(end)
+    edge_cols = sorted([k for k in edges if k not in ('start', 'end')
+                        and len(edges[k]) == len(edges['start'])])
+    edge_vdims = [str(col) if isinstance(col, int) else col for col in edge_cols]
+    edge_data = tuple(edges[col] for col in ['start', 'end']+edge_cols)
+
+    # Unpack user node info
+    xdim, ydim, idim = cls.node_type.kdims[:3]
+    if nodes:
+        node_columns = nodes.columns()
+        idx_dim = nodes.kdims[0].name
+        info_cols, values = zip(*((k, v) for k, v in node_columns.items() if k != idx_dim))
+        node_info = {i: vals for i, vals in zip(node_columns[idx_dim], zip(*values))}
+    else:
+        info_cols = []
+        node_info = None
+    node_columns = defaultdict(list)
+
+    # Unpack node positions
+    for idx, pos in sorted(positions.items()):
+        node = G.nodes.get(idx)
+        if node is None:
+            continue
+        x, y = pos
+        node_columns[xdim.name].append(x)
+        node_columns[ydim.name].append(y)
+        for attr, value in node.items():
+            if isinstance(value, (list, dict)):
+                continue
+            node_columns[attr].append(value)
+        for i, col in enumerate(info_cols):
+            node_columns[col].append(node_info[idx][i])
+        if isinstance(idx, tuple):
+            idx = str(idx) # Tuple node indexes handled as strings
+        node_columns[idim.name].append(idx)
+    node_cols = sorted([k for k in node_columns if k not in cls.node_type.kdims
+                        and len(node_columns[k]) == len(node_columns[xdim.name])])
+    columns = [xdim.name, ydim.name, idim.name]+node_cols+list(info_cols)
+    node_data = tuple(node_columns[col] for col in columns)
+
+    # Construct nodes
+    vdims = []
+    for col in node_cols:
+        if isinstance(col, int):
+            dim = str(col)
+        elif nodes is not None and col in nodes.vdims:
+            dim = nodes.get_dimension(col)
+        else:
+            dim = col
+        vdims.append(dim)
+    nodes = cls.node_type(node_data, vdims=vdims)
+
+    # Construct graph
+    return cls((edge_data, nodes), vdims=edge_vdims)
 
 
 def draw(G, pos=None, **kwargs):
@@ -17,7 +118,6 @@ def draw(G, pos=None, **kwargs):
 
     Draw the graph with hvPlot with options for node positions,
     labeling, titles, and many other drawing features.
-    See draw() for simple drawing without labels or axes.
 
     Parameters
     ----------
@@ -78,32 +178,52 @@ def draw(G, pos=None, **kwargs):
        a string referencing a node attribute
     font_size : int, optional (default=12)
        Font size for text labels
-    font_color : string, optional (default='k' black)
+    font_color : string, optional (default='black')
        Font color string
-    font_weight : string, optional (default='normal')
-       Font weight
     font_family : string, optional (default='sans-serif')
        Font family
     label : string, optional
-        Label for graph legend
+       Label for graph legend
+    selection_policy : string, optional (default='nodes')
+       Whether to select 'nodes', 'edges' or None on tap and selection
+       events.
+    inspection_policy : string, optional (default='nodes')
+       Whether to select 'nodes', 'edges' or None on tap and selection
+       events.
+    geo : boolean, optional (default=False)
+       Whether to return a GeoViews graph
+    crs : cartopy.crs.CRS
+       A cartopy coordinate reference system (enables a geographic plot)
+    height : int, optional (default=400)
+       The height of the plot in pixels
+    width : int, optional (default=400)
+       The width of the plot in pixels
     """
-
-    # Construct Graph object
     if pos is None:
         pos = nx.drawing.spring_layout
 
-    if isinstance(pos, dict):
-        g = Graph.from_networkx(G, nx.circular_layout)
-        if g.nodes.vdims:
-            inds = g.nodes.dimension_values(2)
-            values = g.nodes.array(g.nodes.vdims)
-            lookup = {idx: tuple(vals) for idx, vals in zip(inds, values)}
-        else:
-            lookup = {}
-        nodes = [tuple(v)+(k,)+lookup.get(k, ()) for k, v in pos.items()]
-        g = g.clone((g.data, g.nodes.clone(nodes)))
-    else:
-        g = Graph.from_networkx(G, pos, **kwargs.get('layout_kwargs', {}))
+    if not isinstance(pos, dict):
+        pos = pos(G, **kwargs.get('layout_kwargs', {}))
+
+    params, label_params = {}, {}
+    label_element = Labels
+    if kwargs.get('geo', False) or 'crs' in kwargs:
+        try:
+            import geoviews
+        except ImportError:
+            raise ImportError('In order to use geo-related features '
+                              'the geoviews library must be available. '
+                              'It can be installed with:\n  conda '
+                              'install -c pyviz geoviews')
+        from cartopy import crs as ccrs
+        crs = kwargs.get('crs', ccrs.PlateCarree())
+        label_element = geoviews.Labels
+        params['cls'] = geoviews.Graph
+        params['crs'] = crs
+        label_params['crs'] = crs
+
+    # Construct Graph object
+    g = _from_networkx(G, pos, **params)
 
     if 'nodelist' in kwargs:
         nodelist = list(kwargs['nodelist'])
@@ -111,6 +231,7 @@ def draw(G, pos=None, **kwargs):
         node_alpha = [1 if i in nodelist else 0 for i in inds]
         g = g.clone((g.data, g.nodes.add_dimension('node_alpha', len(g.nodes.vdims), node_alpha, True)))
         kwargs['node_alpha'] = 'node_alpha'
+
     if 'edgelist' in kwargs:
         edges = g.array([0, 1])
         comparisons = []
@@ -119,6 +240,7 @@ def draw(G, pos=None, **kwargs):
         g = g.iloc[np.logical_and(*np.logical_or.reduce(comparisons).T)]
 
     # Compute options
+    inspection_policy = kwargs.pop('inspection_policy', 'nodes')
     opts = dict(
         axiswise=True,
         arrowhead_length=kwargs.get('arrowhead_length', 0.025),
@@ -128,7 +250,7 @@ def draw(G, pos=None, **kwargs):
         width=kwargs.pop('width', 400),
         height=kwargs.pop('height', 400),
         selection_policy=kwargs.pop('selection_policy', 'nodes'),
-        inspection_policy=kwargs.pop('inspection_policy', 'nodes'),
+        inspection_policy=inspection_policy,
         node_fill_color='red')
 
     if '_axis_defaults':
@@ -156,12 +278,14 @@ def draw(G, pos=None, **kwargs):
     if 'style' in kwargs:
         opts['edge_line_dash'] = kwargs.pop('style')
 
-    for node_style in ('node_fill_color', 'node_size', 'node_alpha', 'node_line_width'):
+    node_styles = ('node_fill_color', 'node_size', 'node_alpha', 'node_line_width')
+    for node_style in node_styles:
         if isinstance(opts.get(node_style), (np.ndarray, list, range)):
             g = g.clone((g.data, g.nodes.add_dimension(node_style, len(g.nodes.vdims), opts[node_style], True)))
             opts[node_style] = node_style
 
-    for edge_style in ('edge_line_color', 'edge_line_alpha', 'edge_alpha', 'edge_line_width'):
+    edge_styles = ('edge_line_color', 'edge_line_alpha', 'edge_alpha', 'edge_line_width')
+    for edge_style in edge_styles:
         if isinstance(opts.get(edge_style), (np.ndarray, list, range)):
             g = g.add_dimension(edge_style, len(g.vdims), opts[edge_style], True)
             opts[edge_style] = edge_style
@@ -177,6 +301,14 @@ def draw(G, pos=None, **kwargs):
         if lims != (None, None):
             dimension = g.get_dimension(opts.get('edge_line_color'))
             dimension.range = lims
+
+    if inspection_policy == 'nodes':
+        tooltip_dims = g.nodes.kdims[2:] + g.nodes.vdims
+    else:
+        tooltip_dims = g.kdims + g.vdims
+    tooltips = [(d.label, '@{%s}' % dimension_sanitizer(d.name)) for d in tooltip_dims
+                if d not in node_styles + edge_styles]
+    opts['tools'] = [HoverTool(tooltips=tooltips), 'tap']
 
     g.opts(**opts)
 
@@ -197,9 +329,9 @@ def draw(G, pos=None, **kwargs):
         if isinstance(labels, dict):
             values = g.nodes.array(g.nodes.kdims)
             data = [(x, y, labels[i]) for (x, y, i) in values if i in labels]
-            labels = Labels(data, g.nodes.kdims[:2], 'text')
+            labels = label_element(data, g.nodes.kdims[:2], 'text', **label_params)
         else:
-            labels = Labels(g.nodes, g.nodes.kdims[:2], labels)
+            labels = label_element(g.nodes, g.nodes.kdims[:2], labels, **label_params)
         g = g * labels.opts(**label_opts)
 
     # Apply label
@@ -211,6 +343,10 @@ def draw(G, pos=None, **kwargs):
 
 def draw_networkx(G, pos=None, **kwargs):
     """Draw a networkx graph.
+
+    Draw the graph with hvPlot with options for node positions,
+    labeling, titles, and many other drawing features. See draw() for
+    simple drawing without labels or axes.
 
     Parameters
     ----------
@@ -229,9 +365,10 @@ def draw_networkx(G, pos=None, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
+    graph : holoviews.Graph or holoviews.Overlay
        Graph element
     """
+    kwargs['_axis_defaults'] = False
     return draw(G, pos, **kwargs)
 
 
@@ -249,7 +386,7 @@ def draw_networkx_nodes(G, pos, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
+    graph: holoviews.Graph or holoviews.Overlay
        Graph element
     """
     if 'alpha' in kwargs:
@@ -276,7 +413,7 @@ def draw_networkx_edges(G, pos, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
+    graph : holoviews.Graph or (holoviews.Graph * holoviews.Labels)
        Graph element
     """
     if 'alpha' in kwargs:
@@ -286,6 +423,10 @@ def draw_networkx_edges(G, pos, **kwargs):
     kwargs['node_hover_alpha'] = 0
     kwargs['node_nonselection_alpha'] = 0
     kwargs['_axis_defaults'] = False
+    if kwargs.get('selection_policy') is not None:
+        kwargs['selection_policy'] = 'edges'
+    if kwargs.get('inspection_policy') is not None:
+        kwargs['inspection_policy'] = 'edges'
     return draw(G, pos, **kwargs)
 
 
@@ -303,8 +444,8 @@ def draw_networkx_labels(G, pos, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : Labels element
+       Labels element
     """
     g = draw(G, pos, **kwargs)
     return Labels(g.nodes, g.nodes.kdims[:2], g.nodes.kdims[2])
@@ -324,8 +465,8 @@ def draw_circular(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : holoviews.Graph or holoviews.Overlay
+       Graph element or Graph and Labels
     """
     return draw(G, pos=nx.circular_layout, **kwargs)
 
@@ -344,8 +485,8 @@ def draw_kamada_kawai(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : holoviews.Graph or holoviews.Overlay
+       Graph element or Graph and Labels
     """
     return draw(G, pos=nx.kamada_kawai_layout, **kwargs)
 
@@ -364,7 +505,7 @@ def draw_random(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
+    graph : holoviews.Graph or holoviews.Overlay
        Graph element
     """
     return draw(G, nx.random_layout, **kwargs)
@@ -384,8 +525,8 @@ def draw_shell(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : holoviews.Graph or holoviews.Overlay
+       Graph element or Graph and Labels
     """
     nlist = kwargs.pop('nlist', None)
     if nlist is not None:
@@ -407,8 +548,8 @@ def draw_spectral(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : holoviews.Graph or holoviews.Overlay
+       Graph element or Graph and Labels
     """
     return draw(G, nx.spectral_layout, **kwargs)
 
@@ -427,7 +568,7 @@ def draw_spring(G, **kwargs):
 
     Returns
     -------
-    graph: holoviews.Graph or (holoviews.Graph * holoviews.Labels)
-       Graph element
+    graph : holoviews.Graph or holoviews.Overlay
+       Graph element or Graph and Labels
     """
     return draw(G, nx.spring_layout, **kwargs)
