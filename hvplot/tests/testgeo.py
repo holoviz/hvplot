67622b7b06d4ee687731419b09b7b430759f3c96
Julia Signell <jsignell@gmail.com>
2019-07-17 16:10:56 -0400

    Be explicit about the kind of plot being used when geo=True

--- hvplot/tests/testgeo.py
+++ hvplot/tests/testgeo.py
@@ -20,29 +20,29 @@ class TestGeo(TestCase):
         assert plot.crs.proj4_params['proj'] == proj
 
     def test_plot_with_crs_as_object(self):
-        plot = self.da.hvplot('x', 'y', crs=self.crs)
+        plot = self.da.hvplot.image('x', 'y', crs=self.crs)
         self.assertCRS(plot)
 
     def test_plot_with_crs_as_proj_string(self):
-        plot = self.da.hvplot('x', 'y', crs=self.da.crs)
+        plot = self.da.hvplot.image('x', 'y', crs=self.da.crs)
         self.assertCRS(plot)
 
     def test_plot_with_geo_as_true_crs_undefined(self):
-        plot = self.da.hvplot('x', 'y', geo=True)
+        plot = self.da.hvplot.image('x', 'y', geo=True)
         self.assertCRS(plot)
 
     def test_plot_with_crs_as_attr_str(self):
         da = self.da.copy()
         da.attrs = {'bar': self.crs}
-        plot = da.hvplot('x', 'y', crs='bar')
+        plot = da.hvplot.image('x', 'y', crs='bar')
         self.assertCRS(plot)
 
     def test_plot_with_crs_as_nonexistent_attr_str(self):
         with self.assertRaisesRegex(ValueError, "'foo' must be"):
-            self.da.hvplot('x', 'y', crs='foo')
+            self.da.hvplot.image('x', 'y', crs='foo')
 
     def test_plot_with_geo_as_true_crs_no_crs_on_data_returns_default(self):
         da = self.da.copy()
         da.attrs = {'bar': self.crs}
-        plot = da.hvplot('x', 'y', geo=True)
+        plot = da.hvplot.image('x', 'y', geo=True)
         self.assertCRS(plot, 'eqc')
