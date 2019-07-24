57cde3c041b825d1993270013eb8b000160cd7e9
Philipp Rudiger <prudiger@anaconda.com>
2019-07-24 11:55:40 +0200

    Fixed imports

--- hvplot/tests/testoperations.py
+++ hvplot/tests/testoperations.py
@@ -1,7 +1,9 @@
 from unittest import SkipTest
 from parameterized import parameterized
 
-from datashader.reductions import mean, sum, count_cat
+import numpy as np
+import pandas as pd
+
 from holoviews.element import Image, QuadMesh
 from holoviews.element.comparison import ComparisonTestCase
 
@@ -11,26 +13,29 @@ class TestDatashader(ComparisonTestCase):
 
     def setUp(self):
         try:
-            import pandas as pd
+            import datashader # noqa
         except:
-            raise SkipTest('Pandas not available')
+            raise SkipTest('Datashader not available')
         import hvplot.pandas # noqa
         self.df = pd.DataFrame([[1, 2, 'A', 0.1], [3, 4, 'B', 0.2], [5, 6, 'C', 0.3]],
                                columns=['x', 'y', 'category', 'number'])
 
     def test_rasterize_by_cat(self):
+        from datashader.reductions import count_cat
         dmap = self.df.hvplot.scatter('x', 'y', by='category', rasterize=True)
         agg = dmap.callback.inputs[0].callback.operation.p.aggregator
         self.assertIsInstance(agg, count_cat)
         self.assertEqual(agg.column, 'category')
 
     def test_rasterize_color_dim(self):
+        from datashader.reductions import mean
         dmap = self.df.hvplot.scatter('x', 'y', c='number', rasterize=True)
         agg = dmap.callback.inputs[0].callback.operation.p.aggregator
         self.assertIsInstance(agg, mean)
         self.assertEqual(agg.column, 'number')
 
     def test_rasterize_color_dim_with_string_agg(self):
+        from datashader.reductions import sum
         dmap = self.df.hvplot.scatter('x', 'y', c='number', rasterize=True, aggregator='sum')
         agg = dmap.callback.inputs[0].callback.operation.p.aggregator
         self.assertIsInstance(agg, sum)
@@ -42,9 +47,9 @@ class TestChart2D(ComparisonTestCase):
     def setUp(self):
         try:
             import xarray as xr
-            import numpy as np
+            import datashader as ds # noqa
         except:
-            raise SkipTest('XArray not available')
+            raise SkipTest('xarray or datashader not available')
         import hvplot.xarray  # noqa
         data = np.arange(0, 60).reshape(6, 10)
         x = np.arange(10)
