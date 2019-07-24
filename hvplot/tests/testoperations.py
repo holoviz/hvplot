20ed3f4449c6ae029e7267111a094f02b81a86e8
Philipp Rudiger <prudiger@anaconda.com>
2019-07-24 11:47:32 +0200

    Added datashader agg tests

--- hvplot/tests/testoperations.py
+++ hvplot/tests/testoperations.py
@@ -1,11 +1,44 @@
 from unittest import SkipTest
 from parameterized import parameterized
 
+from datashader.reductions import mean, sum, count_cat
 from holoviews.element import Image, QuadMesh
 from holoviews.element.comparison import ComparisonTestCase
 
 
+
+class TestDatashader(ComparisonTestCase):
+
+    def setUp(self):
+        try:
+            import pandas as pd
+        except:
+            raise SkipTest('Pandas not available')
+        import hvplot.pandas # noqa
+        self.df = pd.DataFrame([[1, 2, 'A', 0.1], [3, 4, 'B', 0.2], [5, 6, 'C', 0.3]],
+                               columns=['x', 'y', 'category', 'number'])
+
+    def test_rasterize_by_cat(self):
+        dmap = self.df.hvplot.scatter('x', 'y', by='category', rasterize=True)
+        agg = dmap.callback.inputs[0].callback.operation.p.aggregator
+        self.assertIsInstance(agg, count_cat)
+        self.assertEqual(agg.column, 'category')
+
+    def test_rasterize_color_dim(self):
+        dmap = self.df.hvplot.scatter('x', 'y', c='number', rasterize=True)
+        agg = dmap.callback.inputs[0].callback.operation.p.aggregator
+        self.assertIsInstance(agg, mean)
+        self.assertEqual(agg.column, 'number')
+
+    def test_rasterize_color_dim_with_string_agg(self):
+        dmap = self.df.hvplot.scatter('x', 'y', c='number', rasterize=True, aggregator='sum')
+        agg = dmap.callback.inputs[0].callback.operation.p.aggregator
+        self.assertIsInstance(agg, sum)
+        self.assertEqual(agg.column, 'number')
+
+
 class TestChart2D(ComparisonTestCase):
+
     def setUp(self):
         try:
             import xarray as xr
