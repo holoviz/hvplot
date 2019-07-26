1a012953e311e1445f214c9ecb19bc79c5766c4c
Julia Signell <jsignell@gmail.com>
2019-07-26 14:10:22 -0400

    Using attrs to set default x and y for gridded outputs (#264)
    * Use axis in attrs to assign to axes
    
    * Adding test

--- hvplot/tests/testutil.py
+++ hvplot/tests/testutil.py
@@ -148,3 +148,17 @@ class TestProcessXarray(TestCase):
         assert y == 'lat'
         assert by is None
         assert groupby == ['time']
+
+    def test_process_3d_xarray_dataset_with_coords_as_gridded_uses_axis_to_get_defaults(self):
+        import xarray as xr
+
+        kwargs = self.default_kwargs
+        kwargs.update(gridded=True)
+
+        data, x, y, by, groupby = process_xarray(data=self.ds, **kwargs)
+        assert isinstance(data, xr.Dataset)
+        assert list(data.data_vars.keys()) == ['air']
+        assert x == 'lon'
+        assert y == 'lat'
+        assert by is None
+        assert groupby == ['time']
