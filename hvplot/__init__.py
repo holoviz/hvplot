1b47e3c57be475d94ec4b2b4f205fa21ac19c370
Jean-Luc Stevens <jlrstevens@gmail.com>
2020-01-28 12:53:02 -0600

    Added support of tab completion via signature (#411)
    * Renamed _get_doc to _get_doc_and_signature
    
    * Generating signature for Python 3 with redundancy filtering
    
    * Minor formatting fix and refactoring

--- hvplot/__init__.py
+++ hvplot/__init__.py
@@ -21,7 +21,7 @@ __version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:
 
 _METHOD_DOCS = {}
 
-def _get_doc(cls, kind, completions=False, docstring=True, generic=True, style=True):
+def _get_doc_and_signature(cls, kind, completions=False, docstring=True, generic=True, style=True):
     converter = HoloViewsConverter
     method = getattr(cls, kind)
     kind_opts = converter._kind_options.get(kind, [])
@@ -49,27 +49,41 @@ def _get_doc(cls, kind, completions=False, docstring=True, generic=True, style=T
     style_opts = 'Style options\n-------------\n\n' + '\n'.join(sorted(valid_opts))
 
     parameters = []
+    extra_kwargs = _hv.core.util.unique_iterator(
+        valid_opts + kind_opts + converter._axis_options + converter._op_options
+    )
+
     if sys.version_info.major < 3:
         argspec = inspect.getargspec(method)
         args = argspec.args[1:]
         defaults = argspec.defaults or [None]*len(args)
         for arg, dflt in zip(args, defaults):
             parameters.append((arg, dflt))
+        signature = None
     else:
         sig = inspect.signature(method)
         for name, p in list(sig.parameters.items())[1:]:
             if p.kind == 1:
                 parameters.append((name, p.default))
-    parameters += [(o, None) for o in
-                   valid_opts+kind_opts+converter._axis_options+converter._op_options]
 
+        filtered_signature = [p for p in sig.parameters.values()
+                              if p.kind != inspect.Parameter.VAR_KEYWORD]
+        extra_params = [inspect.Parameter(k, inspect.Parameter.KEYWORD_ONLY)
+                        for k in extra_kwargs
+                        if k not in [p.name for p in filtered_signature]]
+        all_params = (filtered_signature + extra_params
+                      + [inspect.Parameter('kwargs', inspect.Parameter.VAR_KEYWORD)])
+        signature = inspect.Signature(all_params)
+
+    parameters += [(o, None) for o in extra_kwargs]
     completions = ', '.join(['%s=%s' % (n, v) for n, v in parameters])
     options = textwrap.dedent(converter.__doc__)
     method_doc = _METHOD_DOCS.get(kind, method.__doc__)
     _METHOD_DOCS[kind] = method_doc
-    return formatter.format(
+    docstring = formatter.format(
         kind=kind, completions=completions, docstring=textwrap.dedent(method_doc),
         options=options, style=style_opts)
+    return docstring, signature
 
 
 def help(kind=None, docstring=True, generic=True, style=True):
@@ -88,7 +102,9 @@ def help(kind=None, docstring=True, generic=True, style=True):
     style: boolean (default=True)
         Whether to provide list of style options
     """
-    print(_get_doc(cls=hvPlot, kind=kind, docstring=docstring, generic=generic, style=style))
+    doc, sig = _get_doc_and_signature(cls=hvPlot, kind=kind,
+                                      docstring=docstring, generic=generic, style=style)
+    print(doc)
 
 
 def post_patch(extension='bokeh', logo=False):
@@ -98,11 +114,13 @@ def post_patch(extension='bokeh', logo=False):
 
 def _patch_doc(cls, kind):
     method = getattr(cls, kind)
-    docstring = _get_doc(cls, kind, get_ipy())
     if sys.version_info.major == 2:
+        docstring, signature = _get_doc_and_signature(cls, kind, get_ipy())
         method.__func__.__doc__ = docstring
     else:
+        docstring, signature = _get_doc_and_signature(cls, kind, False)
         method.__doc__ = docstring
+        method.__signature__ = signature
 
 
 # Patch docstrings
