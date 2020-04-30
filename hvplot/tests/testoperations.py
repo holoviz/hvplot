1e24d058eb78af53fd284618b419f7a8ce799dd3
Ian Carroll <itcarroll@users.noreply.github.com>
2020-04-30 09:22:50 -0400

    color_key alias for cmap (#446)

--- hvplot/tests/testoperations.py
+++ hvplot/tests/testoperations.py
@@ -105,6 +105,9 @@ class TestDatashader(ComparisonTestCase):
     def test_cmap_can_be_color_key(self):
         color_key = {'A': '#ff0000', 'B': '#00ff00', 'C': '#0000ff'}
         self.df.hvplot.points(x='x', y='y', by='category', cmap=color_key, datashade=True)
+        with self.assertRaises(TypeError):
+            self.df.hvplot.points(x='x', y='y', by='category', datashade=True,
+                                  cmap='kbc_r', color_key=color_key)
 
     def test_when_datashade_is_true_set_hover_to_false_by_default(self):
         plot = self.df.hvplot(x='x', y='y', datashade=True)
