b1809080c1e6457c66f2baedff6a714b17cb5164
James A. Bednar <jbednar@users.noreply.github.com>
2020-11-17 09:15:28 -0600

    Made a pass over the Interactive user guide (#523)

--- hvplot/pandas.py
+++ hvplot/pandas.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from .interactive  import Interactive
 
-def patch(name='hvplot', extension='bokeh', logo=False):
+def patch(name='hvplot', interactive='interactive', extension='bokeh', logo=False):
     from . import hvPlotTabular, post_patch
 
     try:
@@ -18,9 +18,9 @@ def patch(name='hvplot', extension='bokeh', logo=False):
 
     _patch_interactive = lambda self: Interactive(self)
     _patch_interactive.__doc__ = Interactive.__call__.__doc__
-    interactive_prop = property(_patch_plot)
-    setattr(pd.DataFrame, name, interactive_prop)
-    setattr(pd.Series, name, interactive_prop)
+    interactive_prop = property(_patch_interactive)
+    setattr(pd.DataFrame, interactive, interactive_prop)
+    setattr(pd.Series, interactive, interactive_prop)
 
     post_patch(extension, logo)
 
