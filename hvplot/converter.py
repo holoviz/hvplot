ac2d6ccae41867e27b8a69240d271d06c915eb73
Aaron Spring <aaronspring@users.noreply.github.com>
2021-04-14 14:46:54 +0200

    clim (#587)

--- hvplot/converter.py
+++ hvplot/converter.py
@@ -1821,6 +1821,7 @@ class HoloViewsConverter(object):
         levels = self.kwds.get('levels', 5)
         if isinstance(levels, int):
             opts['color_levels'] = levels
+        opts['clim'] = self._dim_ranges['c']
         return contours(qmesh, filled=filled, levels=levels).opts(**opts)
 
     def contourf(self, x=None, y=None, z=None, data=None):
