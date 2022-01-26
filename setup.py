3270410dd2ae3fa5339f847b9c93b269a461aa5b
maximlt <mliquet@anaconda.com>
2022-01-26 09:47:52 +0100

    install selenium only for the docs build

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ _examples = [
     's3fs',
     'scipy',
     'pillow',
-    'selenium',
     'spatialpandas',
     'scikit-image',
     'python-snappy',
@@ -80,6 +79,7 @@ extras_require = {
     'examples_extra': _examples_extra,
     'doc': _examples_extra + [
         'nbsite >=0.5.1',
+        'selenium',
     ]
 }
 
