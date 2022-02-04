8b728db0a33cca832306c127cbe0db3ac647211f
maximlt <mliquet@anaconda.com>
2022-02-04 21:55:14 +0100

    add module dedicated to transforms and is_interactive_opt util

---
+++ hvplot/tests/testbackend_transforms.py
@@ -0,0 +1,17 @@
+from hvplot.backend_transforms import is_interactive_opt
+
+import pytest
+
+@pytest.mark.parametrize(
+    'bk_option,expected',
+    [
+        ('height', False),
+        ('hover_line_alpha', True)
+        ('nonselection_line_alpha', True),
+        ('muted_line_alpha', True),
+        ('selection_line_alpha', True),
+        ('annular_muted_alpha', True)
+    ]
+)
+def test_is_interactive_opt(bk_option, expected):
+    assert is_interactive_opt(bk_option) == expected
