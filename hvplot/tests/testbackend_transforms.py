a49b0e78c8087ee1ea70d4fa0f37d823296a5d54
maximlt <mliquet@anaconda.com>
2022-02-25 18:08:39 +0100

    add a test for compositeoverlay plots

--- hvplot/tests/testbackend_transforms.py
+++ hvplot/tests/testbackend_transforms.py
@@ -50,6 +50,27 @@ def test_transfer_opts(element, opt, val, backend, opt_kind, transf_opt, transf_
     finally:
         Store.set_current_backend(current_backend)
 
+@pytest.mark.parametrize(
+    ('opt', 'val', 'backend', 'opt_kind', 'transf_opt', 'transf_val'),
+    (
+        ('line_dash', 'dashed', 'matplotlib', 'style', 'linestyle', 'dashed'),
+        ('line_dash', 'dashed', 'plotly', 'style', 'dash', 'dash'),
+    )
+)
+def test_transfer_opts_compositeoverlay(opt, val, backend, opt_kind, transf_opt, transf_val):
+    current_backend = Store.current_backend
+    if backend not in Store.registry:
+        holoviews.extension(backend)
+    Store.set_current_backend(backend)
+    try:
+        element = Area([]) * Curve([]).opts(backend='bokeh', **{opt: val})
+        new_element = element.apply(_transfer_opts, backend=backend)
+        transformed_element = new_element.Curve.I
+        new_opts = transformed_element.opts.get(opt_kind).kwargs
+        assert transf_opt in new_opts
+        assert new_opts[transf_opt] == transf_val
+    finally:
+        Store.set_current_backend(current_backend)
 
 @pytest.mark.parametrize(
     ('bk_option', 'expected'),
