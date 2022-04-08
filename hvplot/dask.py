27d5f2b44a86feeca7eb6d0eacbf70d0171d7303
Philipp Rudiger <prudiger@anaconda.com>
2022-04-08 12:56:09 +0200

    Add ability to call interactive on bound functions (#720)
    * Add ability to call interactive on bound functions
    
    * Add docs
    
    * Select correct Interactive object depending on type
    
    * Add tests
    
    * Remove Interactive.bind
    
    * Update examples/user_guide/Interactive.ipynb
    
    Co-authored-by: James A. Bednar <jbednar@users.noreply.github.com>
    
    * Various fixes
    
    * Fix flake
    
    * Consistently resolve accessors
    
    * Fix docstring generation
    
    Co-authored-by: James A. Bednar <jbednar@users.noreply.github.com>

--- hvplot/dask.py
+++ hvplot/dask.py
@@ -1,6 +1,15 @@
+import sys
+
 from .interactive import Interactive
 
 class DaskInteractive(Interactive):
+
+    @classmethod
+    def applies(cls, obj):
+        if 'dask.dataframe' in sys.modules:
+            import dask.dataframe as dd
+            return isinstance(obj, (dd.Series, dd.DataFrame))
+        return False
 
     def compute(self):
         self._method = 'compute'
