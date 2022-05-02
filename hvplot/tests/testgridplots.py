b14db08271cabfa8bee79b665175c3985f1145d2
maximlt <mliquet@anaconda.com>
2022-05-02 20:34:45 +0200

    add tests

--- hvplot/tests/testgridplots.py
+++ hvplot/tests/testgridplots.py
@@ -191,3 +191,22 @@ class TestGridPlots(ComparisonTestCase):
         assert p.kdims[0].name == 'lon'
         assert p.kdims[1].name == 'lat'
 
+    def test_symmetric_dataset_in_memory(self):
+        # Loading the dataset with cache=True downloads it locally but doesn't
+        # fully load it in memory.
+        ds = xr.tutorial.open_dataset('eraint_uvz', cache=True)
+        plot = ds.u.isel(month=0).hvplot(x='latitude', y='longitude', check_symmetric_max=ds.u.size+1)
+        plot[(ds.u.level.data[0])]
+        plot_opts = Store.lookup_options('bokeh', plot.last, 'plot')
+        # If a DataArray is not in memory, computing whether it's symmetric should
+        # not be done and return False.
+        assert not plot_opts.kwargs['symmetric']
+    
+    def test_symmetric_dataset_not_in_memory(self):
+        # cache=False forces the dataset to be loaded in memory (with ds.load()).
+        ds = xr.tutorial.open_dataset('eraint_uvz', cache=False)
+        plot = ds.u.isel(month=0).hvplot(x='latitude', y='longitude', check_symmetric_max=ds.u.size+1)
+        plot[(ds.u.level.data[0])]
+        plot_opts = Store.lookup_options('bokeh', plot.last, 'plot')
+        # This DataArray happens to be symmetric.
+        assert plot_opts.kwargs['symmetric']
