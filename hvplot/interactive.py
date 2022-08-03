126cde7a77471cda45c2e36fe77de58a887804bc
maximlt <mliquet@anaconda.com>
2022-08-03 12:24:18 +0200

    more comments

--- hvplot/interactive.py
+++ hvplot/interactive.py
@@ -23,21 +23,25 @@ from .util import _flatten, is_tabular, is_xarray, is_xarray_dataarray
 
 def _find_widgets(op):
     widgets = []
-    op_args = list(op['args'])+list(op['kwargs'].values())
+    op_args = list(op['args']) + list(op['kwargs'].values())
     op_args = _flatten(op_args)
     for op_arg in op_args:
+        # Find widgets introduced as `widget` in an expression
         if 'panel' in sys.modules:
             if isinstance(op_arg, Widget) and op_arg not in widgets:
                 widgets.append(op_arg)
+        # TODO: Find how to execute this path?
         if isinstance(op_arg, hv.dim):
             for nested_op in op_arg.ops:
                 for widget in _find_widgets(nested_op):
                     if widget not in widgets:
                         widgets.append(widget)
+        # Find Ipywidgets
         if 'ipywidgets' in sys.modules:
             from ipywidgets import Widget as IPyWidget
             if isinstance(op_arg, IPyWidget) and op_arg not in widgets:
                 widgets.append(op_arg)
+        # Find widgets introduced as `widget.param.value` in an expression
         if (isinstance(op_arg, param.Parameter) and
             isinstance(op_arg.owner, pn.widgets.Widget) and
             op_arg.owner not in widgets):
@@ -47,17 +51,99 @@ def _find_widgets(op):
 
 class Interactive:
     """
-    Interactive is a wrapper around a Python object that lets users create
-    interactive pipelines by calling existing APIs on an object with
-    dynamic parameters or widgets.
+    `Interactive` is a wrapper around a Python object that lets users create
+    interactive pipelines by calling existing APIs on an object with dynamic
+    parameters or widgets.
+
+    `Interactive` can be instantiated with an object:
+    
+    >>> dfi = Interactive(df)
+
+    However the recommended approach is to instantiate it via the
+    `.interactive` accessor that is available on a data structure when it has
+    been patched, e.g. after executing `import hvplot.pandas`.
+
+    >>> dfi = df.interactive
+
+    The `.interactive` accessor can also be called which allows to pass kwargs.
+
+    >>> dfi = df.interactive()
+
+    How it works
+    ------------
+
+    An `Interactive` instance watches what operations are applied to the object.
+
+    To do so, each operation returns a new `Interactive` instance - the creation
+    of a new instance being taken care of by the `_clone` method - which allows
+    the next operation to be recorded, and so on and so forth. E.g. `dfi.head()`
+    first records that the `'head'` attribute is accessed, this is achieved
+    by overriding `__getattribute__`. A new interactive object is returned,
+    which will then record that it is being called, and that will be called as
+    `Interactive` implements `__call__`, which itself returns an `Interactive`
+    instance.
+
+    Note that under the hood even more `Interactive` instances may be created,
+    but this is the gist of it.
+
+    To be able to watch all the potential operations that may be applied to an
+    object, `Interactive` implements on top of `__getattribute__` and
+    `__call__`:
+    
+    - operators such as `__gt__`, `__add__`, etc.
+    - the builtin functions `__abs__` and `__round__`
+    - `__getitem__`
+    - `__array_ufunc__`
+
+    The `_depth` attribute starts at 0 and is incremented by 1 everytime
+    a new `Interactive` instance is created part of a chain.
+    The root instance in an expression has a `_depth` of 0. An expression can
+    consist of multiple chains, such as `dfi[dfi.A > 1]`, as the `Interactive`
+    instance is referenced twice in the expression. As a consequence `_depth`
+    is not the total count of `Interactive` instance creations of a pipeline,
+    it is the count of instances created in outer chain. In the example, that
+    would be `dfi[]`. `Interactive` instances don't have references about
+    the instances that created them or that they create, they just know their
+    current location in a chain thanks to `_depth`. However, as some parameters
+    need to be passed down the whole pipeline, they do have to propagate. E.g.
+    in `dfi.interactive(width=200)`, `width=200` will be propagated as `kwargs`.
+
+    
+    Recording the operations applied to an object in a pipeline is done
+    by gradually building a so-called "dim expression", or "dim transform",
+    which is an expression language provided by HoloViews. dim transform
+    objects are a way to express transforms on `Dataset`s, a `Dataset` being
+    another HoloViews object that is a wrapper around common data structures
+    such as Pandas/Dask/... Dataframes/Series, Xarray Dataset/DataArray, etc.
+    For instance a Python expression such as `(series + 2).head()` can be
+    expressed with a dim transform whose repr will be `(dim('*').pd+2).head(2)`,
+    effectively showing that the dim transfom has recorded the different
+    operations that are meant to be applied to the data.
+    The `_transform` attribute stores the dim transform.
+
+    The `_obj` attribute holds the original data structure that feeds the
+    pipeline. All the `Interactive` instances created while parsing the
+    pipeline share the same `_obj` object. And they all wrap it in a `Dataset`
+    instance, and all apply the current dim transform they are aware of to
+    the original data structure to compute the intermediate state of the data,
+    that is stored it in the `_current` attribute. Doing so is particularly
+    useful in Notebook sessions, as this allows to inspect the transformed
+    object at any point of the pipeline, and as such provide correct
+    auto-completion and docstrings. E.g. executing `dfi.A.max?` in a Notebook
+    will correctly return the docstring of the Pandas Series `.max()` method,
+    as the pipeline evaluates `dfi.A` to hold a current object `_current` that
+    is a Pandas Series, and no longer and DataFrame.
     """
 
+    # TODO: Why?
     __metaclass__ = abc.ABCMeta
 
+    # Hackery to support calls to the classic `.plot` API, see `_get_ax_fn`
+    # for more hacks!
     _fig = None
 
     def __new__(cls, obj, **kwargs):
-        # __new__ implemented to support functions as input, i.e. 
+        # __new__ implemented to support functions as input, e.g. 
         # hvplot.find(foo, widget).interactive().max()
         if 'fn' in kwargs:
             fn = kwargs.pop('fn')
@@ -77,11 +163,17 @@ class Interactive:
 
     @classmethod
     def applies(cls, obj):
+        """
+        Subclasses must implement applies and return a boolean to indicates
+        wheter the subclass should apply or not to the obj.
+        """
         return True
 
     def __init__(self, obj, transform=None, fn=None, plot=False, depth=0,
                  loc='top_left', center=False, dmap=False, inherit_kwargs={},
                  max_rows=100, method=None, **kwargs):
+        # _init is used to prevent to __getattribute__ to execute its
+        # specialized code.
         self._init = False
         if self._fn is not None:
             for _, params in full_groupby(self._fn_params, lambda x: id(x.owner)):
@@ -109,6 +201,8 @@ class Interactive:
         self._loc = loc
         self._center = center
         self._dmap = dmap
+        # TODO: What's the real use of inherit_kwargs? So far I've only seen
+        # it containing 'ax'
         self._inherit_kwargs = inherit_kwargs
         self._max_rows = max_rows
         self._kwargs = kwargs
@@ -128,6 +222,7 @@ class Interactive:
             dinfo = getattr(self._fn.object, '_dinfo', {})
             deps = list(dinfo.get('dependencies', [])) + list(dinfo.get('kw', {}).values())
         else:
+            # TODO: Find how to execute that path?
             parameterized = get_method_owner(self._fn.object)
             deps = parameterized.param.method_dependencies(self._fn.object.__name__)
         return deps
@@ -152,6 +247,7 @@ class Interactive:
                 transform = transform.clone(obj.name)
             obj = transform.apply(ds, keep_index=True, compute=False)
             if self._method:
+                # E.g. `pi = dfi.A` leads to `pi._method` equal to `'A'`.
                 obj = getattr(obj, self._method, obj)
             if self._plot:
                 return Interactive._fig
@@ -168,7 +264,7 @@ class Interactive:
         loc = self._loc if loc is None else loc
         center = self._center if center is None else center
         dmap = self._dmap if dmap is None else dmap
-        depth = self._depth+1
+        depth = self._depth + 1
         if copy:
             kwargs = dict(self._kwargs, inherit_kwargs=self._inherit_kwargs, method=self._method, **kwargs)
         else:
@@ -193,7 +289,11 @@ class Interactive:
 
     def _resolve_accessor(self):
         if not self._method:
+            # No method is yet set, as in `dfi.A`, so return a copied clone.
             return self._clone(copy=True)
+        # This is executed when one runs e.g. `dfi.A > 1`, in which case after
+        # dfi.A the _method 'A' is set (in __getattribute__) which allows
+        # _resolve_accessor to keep building the transform dim expression.
         transform = type(self._transform)(self._transform, self._method, accessor=True)
         transform._ns = self._current
         inherit_kwargs = {}
@@ -202,6 +302,8 @@ class Interactive:
         try:
             new = self._clone(transform, inherit_kwargs=inherit_kwargs)
         finally:
+            # Reset _method for whatever happens after the accessor has been
+            # fully resolved, e.g. whatever happens `dfi.A > 1`.
             self._method = None
         return new
 
@@ -214,9 +316,13 @@ class Interactive:
         method = self_dict['_method']
         if method:
             current = getattr(current, method)
+        # Getting all the public attributes available on the current object,
+        # e.g. `sum`, `head`, etc.
         extras = [d for d in dir(current) if not d.startswith('_')]
         if name in extras and name not in super().__dir__():
             new = self._resolve_accessor()
+            # Setting the method name for a potential use later by e.g. an
+            # operator or method, as in `dfi.A > 2`. or `dfi.A.max()`
             new._method = name
             try:
                 new.__doc__ = getattr(current, name).__doc__
@@ -239,9 +345,16 @@ class Interactive:
     def __call__(self, *args, **kwargs):
         if self._method is None:
             if self._depth == 0:
+                # This code path is entered when initializing an interactive
+                # class from the accessor, e.g. with df.interactive(). As
+                # calling the accessor df.interactive already returns an
+                # Interactive instance.
                 return self._clone(*args, **kwargs)
+            # TODO: When is this error raised?
             raise AttributeError
         elif self._method == 'plot':
+            # This - {ax: get_ax} - is passed as kwargs to the plot method in
+            # the dim expression.
             kwargs['ax'] = self._get_ax_fn()
         new = self._clone(copy=True)
         try:
@@ -249,6 +362,8 @@ class Interactive:
             kwargs = dict(new._inherit_kwargs, **kwargs)
             clone = new._clone(method(*args, **kwargs), plot=new._method == 'plot')
         finally:
+            # If an error occurs reset _method anyway so that, e.g. the next
+            # attempt in a Notebook, is set appropriately.
             new._method = None
         return clone
 
@@ -257,6 +372,7 @@ class Interactive:
     #----------------------------------------------------------------
 
     def __array_ufunc__(self, *args, **kwargs):
+        # TODO: How to trigger this method?
         new = self._resolve_accessor()
         transform = new._transform
         transform = args[0](transform, *args[3:], **kwargs)
@@ -294,6 +410,7 @@ class Interactive:
         other = other._transform if isinstance(other, Interactive) else other
         return self._apply_operator(operator.and_, other)
     def __div__(self, other):
+        # TODO: operator.div is only available in Python 2, to be removed.
         other = other._transform if isinstance(other, Interactive) else other
         return self._apply_operator(operator.div, other)
     def __eq__(self, other):
@@ -385,6 +502,7 @@ class Interactive:
         return self._apply_operator(operator.getitem, other)
 
     def _plot(self, *args, **kwargs):
+        # TODO: Seems totally unused to me, as self._plot is set to a boolean in __init__
         @pn.depends()
         def get_ax():
             from matplotlib.backends.backend_agg import FigureCanvas
@@ -411,7 +529,7 @@ class Interactive:
 
     def eval(self):
         """
-        Returns the currrent state of the interactive expression. The
+        Returns the current state of the interactive expression. The
         returned object is no longer interactive.
         """
         obj = self._current
@@ -440,6 +558,7 @@ class Interactive:
             widgets = Column(widget_box, VSpacer())
         elif loc in ('left_bottom', 'right_bottom'):
             widgets = Column(VSpacer(), widget_box)
+        # TODO: add else and raise error
         center = self._center
         if not widgets:
             if center:
