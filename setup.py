2ec3a858c2c0cb8d37e3f52fafec029a679dfa44
Simon Høxbro Hansen <simon.hansen@me.com>
2023-08-29 13:56:36 +0200

    Undebug

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ extras_require['examples'] = [
     'numba >=0.51.0',
     'datashader >=0.6.5',
     'spatialpandas >=0.4.3',
-    'hdf5 ==1.14.0',
+    'hdf5 !=1.14.1',  # Gives coredump in test suite on Linux and Mac
 ]
 
 # Run the example tests by installing examples_tests together with tests
