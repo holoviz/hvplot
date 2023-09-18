f1e19063df5814223d7ddec67577aff206a2d8b0
Andrew Huang <ahuang@anaconda.com>
2023-09-18 08:27:57 -0700

    Fix test

--- hvplot/tests/testcharts.py
+++ hvplot/tests/testcharts.py
@@ -3,6 +3,7 @@ import numpy as np
 from unittest import SkipTest, expectedFailure
 from parameterized import parameterized
 
+from holoviews.core.dimension import Dimension
 from holoviews import NdOverlay, Store, dim, render
 from holoviews.element import Curve, Area, Scatter, Points, Path, HeatMap
 from holoviews.element.comparison import ComparisonTestCase
@@ -349,9 +350,8 @@ class TestChart1D(ComparisonTestCase):
 
     def test_labels_format(self):
         plot = self.df.hvplot("x", "y", text="({x}, {y})", kind="labels")
-        print(plot)
-        # assert list(plot.dimensions()) == ['x', 'y', 'labels']
-        # assert plot.data['labels'].tolist() == ['(1, 2)', '(3, 4)', '(5, 6)']
+        assert list(plot.dimensions()) == [Dimension('x'), Dimension('y'), Dimension('label')]
+        assert list(plot.data["label"]) == ['(1, 2)', '(3, 4)', '(5, 6)']
 
 class TestChart1DDask(TestChart1D):
 
