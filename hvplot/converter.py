b03de63ffc4bb92fd97943a48f2ed1af515323a3
Andrew Huang <ahuang@anaconda.com>
2023-10-04 08:32:04 -0700

    CLeanup

--- hvplot/converter.py
+++ hvplot/converter.py
@@ -343,7 +343,7 @@ class HoloViewsConverter:
         'step': Curve,
         'table': Table,
         'vectorfield': VectorField,
-        'violin': Violin,
+        'violin': Violin
     }
 
     # Types which have a colorbar by default
@@ -488,7 +488,7 @@ class HoloViewsConverter:
         self.stacked = stacked
 
         plot_opts = dict(self._default_plot_opts,
-                         **self._process_plot(kwds))
+                         **self._process_plot())
         if xlim is not None:
             plot_opts['xlim'] = tuple(xlim)
         if ylim is not None:
@@ -983,14 +983,8 @@ class HoloViewsConverter:
                     param.main.param.warning('Unable to auto label using xarray attrs '
                                        f'because {e}')
 
-    def _replace_explorer_kind(self, kwds):
-        kind = kwds.get("_explorer_kind", self.kind)
-        if kind == "explorer":
-            kind = "line"
-        return kind
-
     def _process_plot(self, kwds):
-        kind = self._replace_explorer_kind(kwds)
+        kind = self.kind
         options = Store.options(backend='bokeh')
         elname = self._kind_mapping[kind].__name__
         plot_opts = options[elname].groups['plot'].options if elname in options else {}
@@ -1006,7 +1000,7 @@ class HoloViewsConverter:
         return plot_opts
 
     def _process_style(self, kwds, plot_opts):
-        kind = self._replace_explorer_kind(kwds)
+        kind = self.kind
         eltype = self._kind_mapping[kind]
         registry =  Store.registry[self._backend_compat]
 
@@ -1116,15 +1110,15 @@ class HoloViewsConverter:
         return style_opts, plot_opts, kwds
 
     def _validate_kwds(self, kwds):
-        kind = self._replace_explorer_kind(kwds)
-        kind_opts = self._kind_options.get(kind, [])
+        kind_opts = self._kind_options.get(self.kind, [])
+        kind = self.kind
         eltype = self._kind_mapping[kind]
         if eltype in Store.registry[self._backend_compat]:
             valid_opts = Store.registry[self._backend_compat][eltype].style_opts
         else:
             valid_opts = []
         ds_opts = ['max_px', 'threshold']
-        mismatches = sorted(k for k in kwds if k not in kind_opts + ds_opts + valid_opts)
+        mismatches = sorted(k for k in kwds if k not in kind_opts+ds_opts+valid_opts)
         if not mismatches:
             return
 
@@ -1161,8 +1155,6 @@ class HoloViewsConverter:
     def __call__(self, kind, x, y):
         kind = self.kind or kind
         method = getattr(self, kind)
-        if kind == "explorer":
-            method = partial(method, self.data)
 
         groups = self.groupby
         zs = self.kwds.get('z', [])
