290a7a241fbbcb46901ab97a6b59ae90527e73ca
maximlt <mliquet@anaconda.com>
2023-11-13 14:40:33 +0100

    clean up and set column name explicitely

--- examples/conftest.py
+++ examples/conftest.py
@@ -1,13 +1,1 @@
-import numpy as np
-
-from packaging.version import Version
-
-
 collect_ignore_glob = []
-
-# 2023-11: Newer version of numpy renames amin/amax to min/max
-# This can be removed when we drop Python 3.8 support
-if Version(np.__version__) < Version("1.26"):
-    collect_ignore_glob += [
-        "user_guide/Plotting*.ipynb",
-    ]
