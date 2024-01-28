9c12984e749c75b530149deb20ac27ebdd72b4bb
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-01-28 09:44:48 +0000

    add downstream polars test (#1243)

--- hvplot/tests/plotting/testcore.py
+++ hvplot/tests/plotting/testcore.py
@@ -1,3 +1,6 @@
+from packaging.version import Version
+from unittest import SkipTest
+
 import numpy as np
 import pandas as pd
 import hvplot.pandas  # noqa
@@ -88,3 +91,12 @@ def test_series_polars(kind):
     ser = pl.Series(values=np.random.rand(10), name="A")
     assert isinstance(ser, pl.Series)
     ser.hvplot(kind=kind)
+
+@pytest.mark.skipif(skip_polar, reason="polars not installed")
+@series_kinds
+def test_series_polars_downstream(kind):
+    if Version(pl.__version__) < Version('0.20.3'):
+        raise SkipTest('plot namespace in Polars introduced in 0.20.3')
+    ser = pl.Series(values=np.random.rand(10), name="A")
+    assert isinstance(ser, pl.Series)
+    ser.plot(kind=kind)
