d9f034a2523790de8af4a88110ba76e428a727ae
maximlt <mliquet@anaconda.com>
2024-03-21 22:40:10 +0100

    Revert "conditionally skip ibis test"
    This reverts commit b1808db80681d549f1cefbe63fd476991a81fb3c.

--- hvplot/tests/testibis.py
+++ hvplot/tests/testibis.py
@@ -4,9 +4,6 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from hvplot.util import hv_version
-from packaging.version import Version
-
 try:
     import hvplot.ibis  # noqa
     import ibis
@@ -14,7 +11,6 @@ except ImportError:
     pytest.skip(allow_module_level=True)
 
 
-@pytest.mark.skipif(hv_version <= Version("1.17"), reason="Not supported")
 def test_ibis_hist():
     df = pd.DataFrame(dict(x=np.arange(10)))
     table = ibis.memtable(df)
