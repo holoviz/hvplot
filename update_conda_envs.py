19b830dca481b8c78a2038dae0123316867ce448
maximlt <mliquet@anaconda.com>
2024-03-26 00:19:55 +0100

    install ibis-duckdb on Python 3.12 with conda

--- update_conda_envs.py
+++ update_conda_envs.py
@@ -16,15 +16,3 @@ for file in Path('envs').glob('*.yaml'):
         f.write('  - pip\n')
         f.write('  - pip:\n')
         f.write('    - -e ..\n')
-
-# Remove ibis-duck from the Python 3.12 env since it's python-duck
-# is not yet available on Python 3.12
-# https://github.com/conda-forge/python-duckdb-feedstock/issues/98
-fname = "envs/py3.12-tests.yaml"
-with open(fname, "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-modified_lines = [line for line in lines if "ibis-duckdb" not in line]
-
-with open(fname, "w") as f:
-    f.writelines(modified_lines)
