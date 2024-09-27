b36e3a15168d9ca5f5370337be39b568de5e3add
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2024-09-27 15:01:35 +0200

    Fix the docs build (#1424)

--- doc/conf.py
+++ doc/conf.py
@@ -113,6 +113,8 @@ html_context.update(  # noqa
 nb_execution_excludepatterns = [
     'user_guide/Streaming.ipynb',
 ]
+# cell execution timeout in seconds (-1 to ignore, 30 by default)
+nb_execution_timeout = 120
 
 if os.getenv('HVPLOT_REFERENCE_GALLERY') not in ('False', 'false', '0'):
     rediraffe_redirects.update(
