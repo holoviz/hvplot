7dffe219ba95beb59fb264d464774a1d91b9205b
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-01-27 12:36:15 +0100

    redirect removed pages

--- doc/conf.py
+++ doc/conf.py
@@ -101,6 +101,11 @@ rediraffe_redirects = {
     'developer_guide/testing': 'developer_guide',
     # Removal of the developer_guide folder
     'developer_guide/index': 'developer_guide',
+    # Redirecting removed "getting started" pages to the new index
+    'getting_started/explorer': 'getting_started/index',
+    'getting_started/hvplot': 'getting_started/index',
+    'getting_started/installation': 'getting_started/index',
+    'getting_started/interactive': 'getting_started/index',
 }
 
 html_context.update(  # noqa
