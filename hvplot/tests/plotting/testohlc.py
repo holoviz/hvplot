46e53d0e0c521aa3e3a1ef1610db47967c9b46b2
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-02-06 16:28:47 +0100

    added tests for non-datetime x-axis

--- hvplot/tests/plotting/testohlc.py
+++ hvplot/tests/plotting/testohlc.py
@@ -44,3 +44,27 @@ def test_ohlc_date_tooltip_format():
     formatter_key = '@' + x_label
     formatter = hover_tool.formatters
     assert formatter.get(formatter_key) == 'datetime'
+
+
+def test_ohlc_non_datetime_x_axis():
+    df = pd.DataFrame(
+        {
+            'Open': [100.00, 101.25, 102.75],
+            'High': [104.10, 105.50, 110.00],
+            'Low': [94.00, 97.10, 99.20],
+            'Close': [101.15, 99.70, 109.50],
+            'Volume': [10012, 5000, 18000],
+        },
+        index=[1, 2, 3],
+    )
+
+    ohlc_cols = ['Open', 'High', 'Low', 'Close']
+
+    plot = df.hvplot.ohlc(y=ohlc_cols)
+    segments = plot.Segments.I
+    hover_tool = segments.opts.get('plot').kwargs['tools'][0]
+    tooltips = hover_tool.tooltips
+    x_label, x_tooltip = tooltips[0]
+    assert '{%F}' not in x_tooltip
+    formatter_key = '@' + x_label
+    assert formatter_key not in hover_tool.formatters
