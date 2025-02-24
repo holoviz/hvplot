694489672f0ee98989a67bd416290ea4b9378527
Azaya <99359668+Azaya89@users.noreply.github.com>
2025-02-24 19:53:53 +0100

    Document keywords not in the HoloViewsConverter docstring (#1511)

--- hvplot/converter.py
+++ hvplot/converter.py
@@ -128,25 +128,43 @@ class HoloViewsConverter:
     autorange (default=None): Literal['x', 'y'] | None
         Whether to enable auto-ranging along the x- or y-axis when
         zooming. Requires HoloViews >= 1.16.
+    backlog (default=1000): int
+        Maximum number of rows to keep in the stream buffer when using a streaming data source.
     bgcolor (default=None): str
         Background color of the data area of the plot
+    by (default=None): str or list of str
+        Dimension(s) by which to group the data categories.
+        An NdOverlay is returned by default unless `subplots=True`, then an NdLayout is returned.
     clim: tuple
         Lower and upper bound of the color scale
     cnorm (default='linear'): str
         Color scaling which must be one of 'linear', 'log' or 'eq_hist'
+    col (default=None): str
+        Column name to use for splitting the plot into separate subplots by columns.
     colorbar (default=False): boolean
         Enables a colorbar
+    fields (default={}): dict
+        A dictionary of fields for renaming or transforming data dimensions.
+    flip_xaxis/flip_yaxis: boolean
+        Whether to flip the axis left to right or up and down respectively
     fontscale: number
         Scales the size of all fonts by the same amount, e.g. fontscale=1.5
         enlarges all fonts (title, xticks, labels etc.) by 50%
     fontsize: number or dict
         Set title, label and legend text to the same fontsize. Finer control
         by using a dict: {'title': '15pt', 'ylabel': '5px', 'ticks': 20}
-    flip_xaxis/flip_yaxis: boolean
-        Whether to flip the axis left to right or up and down respectively
+    framewise (default=True): boolean
+        Whether to compute the axis ranges frame-by-frame when using dynamic plots.
     grid (default=False): boolean
         Whether to show a grid
-    hover : boolean
+    group_label (default=None): str
+        Label for grouped data, typically used in legends or as axis labels.
+    groupby (default=None): str or list of str
+        Dimension(s) by which to group data, enabling widgets.
+        Returns a DynamicMap if `dynamic=True`, else returns a HoloMap.
+    kind (default='line'): str
+        The type of plot to generate.
+    hover: boolean
         Whether to show hover tooltips, default is True unless datashade is
         True in which case hover is False by default
     hover_cols (default=[]): list or str
@@ -160,6 +178,8 @@ class HoloViewsConverter:
         Swaps x- and y-axis
     frame_width/frame_height: int
         The width and height of the data area of the plot
+    label (default=None): str
+        Label for the data, typically used in the plot title or legends.
     legend (default=True): boolean or str
         Whether to show a legend, or a legend position
         ('top', 'bottom', 'left', 'right')
@@ -179,6 +199,8 @@ class HoloViewsConverter:
         specify different amount of padding for x- and y-axis and
         tuples of tuples to specify different amounts of padding for
         upper and lower bounds.
+    persist (default=False): boolean
+        Whether to persist the data in memory when using dask.
     rescale_discrete_levels (default=True): boolean
         If `cnorm='eq_hist'` and there are only a few discrete values,
         then `rescale_discrete_levels=True` (the default) decreases
@@ -201,6 +223,14 @@ class HoloViewsConverter:
     rot: number
         Rotates the axis ticks along the x-axis by the specified
         number of degrees.
+    row (default=None): str
+        Column name to use for splitting the plot into separate subplots by rows.
+    stacked (default=False): boolean
+        Whether to stack data in plots like `bar`, `barh` and `area`.
+    stream (default=None): holoviews.streams.Stream or None
+        A stream object for dynamic plotting, allowing data updates without re-rendering the entire plot.
+    subplots (default=False): boolean
+        Whether to display data in separate subplots when using the `by` parameter.
     subcoordinate_y: bool or dict
        Whether to enable sub-coordinate y systems for this plot. Accepts also a
        dictionary of related options to pass down to HoloViews,
@@ -213,6 +243,13 @@ class HoloViewsConverter:
         Title for the plot
     tools (default=[]): list
         List of tool instances or strings (e.g. ['tap', 'box_select'])
+    use_dask (default=False): boolean
+        Whether to use dask for processing the data, helpful for large datasets that do not fit into memory.
+    use_index (default=True): boolean
+        Whether to use the data's index for the x-axis by default.
+        if `hover_cols == 'all', adds the index to the hover tools.
+    value_label (default='value'): str
+        Label for the data values, typically used for the y-axis or in legends.
     xaxis/yaxis: str or None
         Whether to show the x/y-axis and whether to place it at the
         'top'/'bottom' and 'left'/'right' respectively.
@@ -291,6 +328,8 @@ class HoloViewsConverter:
        the browser information is not available. Useful when the browser
        information is not available (pixel_ratio=2 can give better results on
        Retina displays) or for using lower resolution for speed.
+    precompute (default=False): boolean
+        Whether to precompute aggregations when using `rasterize` or `datashade`.
     rasterize (default=False):
         Whether to apply rasterization using the Datashader library,
         returning an aggregated Image (to be colormapped by the
