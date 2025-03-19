7276846ef75df685a7b99981c6d80244845bfd4b
maximlt <mliquet@anaconda.com>
2025-03-19 18:29:03 +0100

    parallelize the docs build

--- doc/_ext/plotting_options_table.py
+++ doc/_ext/plotting_options_table.py
@@ -51,6 +51,12 @@ class PlottingOptionsTableDirective(SphinxDirective):
 def setup(app):
     app.add_directive('plotting-options-table', PlottingOptionsTableDirective)
 
+    return {
+        'version': '0.1',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
+
 
 if __name__ == '__main__':
     group = ' '.join(sys.argv[1:])
