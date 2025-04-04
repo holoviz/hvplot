8e448fc5dcb8a8eefc47d207ff4f88c3ebddfe1f
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-04-04 18:30:32 +0100

    update 'group_label' docstring

--- hvplot/converter.py
+++ hvplot/converter.py
@@ -145,7 +145,13 @@ class HoloViewsConverter:
         Returns a DynamicMap if ``dynamic=True``, else returns a HoloMap.
         See ``dynamic`` for more information.
     group_label : str or None, default=None
-        Label for grouped data, typically in legends or axis labels.
+        A custom label for the grouping dimension used in overlay plots.
+        When multiple series are plotted, hvPlot creates an overlay (NdOverlay)
+        with a key dimension that labels each series. By default, this label is set
+        to 'Variable'. Setting `group_label` overrides this default, allowing you to
+        provide a more descriptive name for the grouping dimension.
+        Note that `group_label` only affects the overlay’s key dimension when multiple
+        groups (or series) are present.
     kind : str, default='line'
         The type of plot to generate.
     label : str or None, default=None
