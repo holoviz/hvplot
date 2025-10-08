4817f15f21240aab25df9736249bfa1348d67364
maximlt <mliquet@anaconda.com>
2025-10-08 09:54:48 +0200

    skip notebooks failing tests because of a dead kernel

--- doc/conftest.py
+++ doc/conftest.py
@@ -74,3 +74,28 @@ else:
         collect_ignore_glob += [
             'user_guide/Gridded_Data.ipynb',
         ]
+
+
+def pytest_runtest_makereport(item, call):
+    """
+    Skip tests that fail because "the kernel died before replying to kernel_info"
+    this is a common error when running the example tests in CI.
+
+    Inspired from: https://stackoverflow.com/questions/32451811
+
+    """
+    from _pytest.runner import pytest_runtest_makereport
+
+    tr = pytest_runtest_makereport(item, call)
+
+    if call.excinfo is not None:
+        msgs = [
+            'Kernel died before replying to kernel_info',
+            "Kernel didn't respond in 60 seconds",
+        ]
+        for msg in msgs:
+            if call.excinfo.type is RuntimeError and call.excinfo.value.args[0] in msg:
+                tr.outcome = 'skipped'
+                tr.wasxfail = f'reason: {msg}'
+
+    return tr
