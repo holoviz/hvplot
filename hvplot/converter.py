58ecac37fed12104f5192b77c05fa0d046d57058
Andrew <15331990+ahuang11@users.noreply.github.com>
2025-10-24 05:06:08 -0700

    Automatically use y as default 'text' kwarg if only two cols (#1681)

--- hvplot/converter.py
+++ hvplot/converter.py
@@ -3038,7 +3038,7 @@ class HoloViewsConverter:
 
         text = self.kwds.get('text')
         if not text:
-            text = [c for c in data.columns if c not in (x, y)][0]
+            text = next((c for c in data.columns if c not in (x, y)), y)
         elif text not in data.columns:
             data = data.copy()
             template_str = text  # needed for dask lazy compute
