1411a7be9139b9b708e299f60e19ad0a68164ff8
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-03-27 08:21:48 +0100

    ci: Add retry option to download data to avoid failures

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,10 +1,25 @@
+import sys
+import time
+
 import bokeh
 from packaging.version import Version
+
+
+def retry(func, *args, **kwargs):
+    for i in range(5):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            wait = 10 * 2**i
+            print(f'Attempt {i + 1} failed: {e}. Retrying in {wait}s...', file=sys.stderr)
+            time.sleep(wait)
+    return func(*args, **kwargs)
+
 
 if Version(bokeh.__version__).release < (3, 5, 0):
     import bokeh.sampledata
 
-    bokeh.sampledata.download()
+    retry(bokeh.sampledata.download)
     print('bokeh data downloaded.')
 
 try:
@@ -12,8 +27,8 @@ try:
     import scipy  # noqa: F401
     import xarray as xr
 
-    xr.tutorial.open_dataset('air_temperature')
-    xr.tutorial.open_dataset('rasm')
+    retry(xr.tutorial.open_dataset, 'air_temperature')
+    retry(xr.tutorial.open_dataset, 'rasm', decode_times=False)
     print('xarray data downloaded.')
 except ModuleNotFoundError as e:
     print(f'ModuleNotFoundError when attempting to download xarray datasets : {e}')
